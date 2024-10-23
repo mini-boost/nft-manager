0e04ca99a7f14cd2b8147825aa9cd233c5c317bd
docker <docker.web3@gmail.com>
2024-10-23 20:22:38 +0800

    代码优化：命令行参数形式输入

--- scripts/nft_create/nft_create.py
+++ scripts/nft_create/nft_create.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import shutil
 import random
@@ -8,6 +9,15 @@ def create_nft_by_season_template(nft_template_dir, season_id, nft_id):
     # 确保目标目录存在
     target_dir = os.path.join(TARGET_BASE_DIR, season_id)
     os.makedirs(target_dir, exist_ok=True)
+
+    # 赛季目录 season_{season_id}
+    nft_template_dir = os.path.join(nft_template_dir, 'season_'+season_id)
+
+    # 判断赛季模板目录是否存在，不存在报错退出
+    if not os.path.exists(nft_template_dir):
+        print(f"赛季模板目录 {nft_template_dir} 不存在。")
+        return
+    print(f"使用模板目录 {nft_template_dir}")
 
     # 从源目录中选择未标记为使用的文件
     files = [f for f in os.listdir(nft_template_dir)
@@ -37,7 +47,14 @@ def create_nft_by_season_template(nft_template_dir, season_id, nft_id):
 
 # test
 if __name__ == '__main__':
-    nft_template_directory = 'D:\\code\\nft-manager\\nft_template'
-    season_id = '1'
-    nft_id = '12345'
-    create_nft_by_season_template(nft_template_directory, season_id, nft_id)
+    # 设置命令行参数解析
+    parser = argparse.ArgumentParser(description='创建 NFT 按赛季模板')
+    parser.add_argument('nft_template_dir', type=str, help='NFT 模板目录')
+    parser.add_argument('season_id', type=str, help='赛季 ID')
+    parser.add_argument('nft_id', type=str, help='NFT ID')
+
+    # 解析命令行参数
+    args = parser.parse_args()
+
+    # 调用创建 NFT 的函数
+    create_nft_by_season_template(args.nft_template_dir, args.season_id, args.nft_id)
